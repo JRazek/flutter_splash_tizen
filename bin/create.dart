3f1ad9fd9c01bebdf0be182e7b545d8e0912d016
JRazek <jakub.jakubecky123@gmail.com>
2021-11-02 23:44:14 +0100

    create

--- bin/create.dart
+++ bin/create.dart
@@ -1,35 +1,8 @@
-import 'dart:io';
-import 'package:yaml/yaml.dart';
+import 'file_utils.dart';
 import 'package:xml/xml.dart';
-import 'package:image/image.dart' as img;
-
-Map? loadYamlFileSync(String path) {
-  File file = File(path);
-  if (file.existsSync() == true) {
-    return loadYaml(file.readAsStringSync());
-  }
-  return null;
-}
-
-XmlDocument loadXMLFileSync(String path) {
-  File file = File(path);
-  if (file.existsSync() == true) {
-    return XmlDocument.parse(file.readAsStringSync());
-  }
-  throw FormatException("could not read $path");
-}
-
-void writeToFileSync(String path, String data) {
-  File file = File(path);
-  if (file.existsSync() == true) {
-    file.writeAsStringSync(data);
-  } else {
-    throw FormatException("could not write to $path");
-  }
-}
 
 void main() {
-  var doc = loadYamlFileSync("pubspec.yaml")?['flutter_native_splash'];
+  var doc = loadYamlFileSync("pubspec.yaml")?['flutter_native_splash_tizen'];
   String color = doc["color"] ?? ""; //[TODO] generation of image in this color?
   String image = doc["image"] ?? "";
   bool tizenFlag = doc["tizen"] ?? false;
@@ -58,6 +31,4 @@ void main() {
   splashScreens.children.add(splashScreen);
 
   writeToFileSync(tizenManifestPath, el.toXmlString());
-  // print(el.children);
-  // print(el.toXmlString());
 }
